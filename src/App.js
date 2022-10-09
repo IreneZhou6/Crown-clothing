c7fa3b64e01978d6ea7c1fbe0eae554f1cb8b157
Irene Z <2511486093@qq.com>
2022-10-09 20:45:25 +0800

    some change

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import Directory from './component/directory/directory.component';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/home/home.component'
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://robohash.org/1?set=set4&size=340x340',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://robohash.org/2?set=set4&size=340x340',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://robohash.org/3?set=set4&size=340x340',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://robohash.org/4?set=set4&size=340x340',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://robohash.org/5?set=set4&size=340x340',
-    },
-  ]
   return (
-    <Directory categories={categories} />
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,36 @@
+import Directory from '../../component/directory/directory.component';
+
+const Home = () => {
+    const categories = [
+        {
+            id: 1,
+            title: 'hats',
+            imageUrl: 'https://robohash.org/1?set=set4&size=340x340',
+        },
+        {
+            id: 2,
+            title: 'jackets',
+            imageUrl: 'https://robohash.org/2?set=set4&size=340x340',
+        },
+        {
+            id: 3,
+            title: 'sneakers',
+            imageUrl: 'https://robohash.org/3?set=set4&size=340x340',
+        },
+        {
+            id: 4,
+            title: 'womens',
+            imageUrl: 'https://robohash.org/4?set=set4&size=340x340',
+        },
+        {
+            id: 5,
+            title: 'mens',
+            imageUrl: 'https://robohash.org/5?set=set4&size=340x340',
+        },
+    ]
+    return (
+        <Directory categories={categories} />
+    );
+}
+
+export default Home;
